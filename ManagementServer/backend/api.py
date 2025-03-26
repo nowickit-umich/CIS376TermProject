92e0b7a1a162c0ee5f11f024dd372a4fd32a9c7e
unknown <nowickit@umich.edu>
2025-03-25 20:29:57 -0400

    Added example backend to database API definition

--- ManagementServer/backend/api.py
+++ ManagementServer/backend/api.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pymysql
 
 app = Flask(__name__)
+
+db_config = {
+    "host": "database",
+    "user": "user",
+    "password": "pass",
+    "database": "app_db"
+}
 
 @app.route("/test", methods=['GET'])
 def test():
     return "This is a test!"
+
+@app.route("/dbtest", methods=['GET'])
+def dbtest():
+    connection = None
+    try:
+        connection = pymysql.connect(**db_config, cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM endpoints;")
+            result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    finally:
+        if connection:
+            connection.close()
 
 
 
