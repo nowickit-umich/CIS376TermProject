6fbc5e42f98b6812b26732240970cf8899a9212f
David Corona <davico@umich.com>
2025-03-26 21:35:07 -0700

    Set up hot reloading and dev config for Docker

--- ManagementServer/frontend/dashboard/next.config.mjs
+++ ManagementServer/frontend/dashboard/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { dev }) => {
+      if (dev) {
+        config.watchOptions = {
+          poll: 1000,
+          aggregateTimeout: 300,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
