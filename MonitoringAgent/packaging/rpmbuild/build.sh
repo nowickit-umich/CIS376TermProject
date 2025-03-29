3874f7ae970dc42716e6979ceb59e4ec83e8703f
unknown <nowickit@umich.edu>
2025-03-28 20:28:57 -0400

    update build script

--- MonitoringAgent/packaging/rpmbuild/build.sh
+++ MonitoringAgent/packaging/rpmbuild/build.sh
@@ -2,11 +2,11 @@
 
 set -e
 sudo dnf install -y rpm-build
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cp -r ./src/* ~/rpmbuild/SOURCES/
+mkdir -p ./packaging/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp -r ./src/* ./packaging/rpmbuild/SOURCES/
 
-rpmbuild -bb ~/rpmbuild/SPECS/monitoring_agent.spec
+rpmbuild -bb --buildroot ./packaging/rpmbuild/BUILD/ ./packaging/rpmbuild/SPECS/monitoring_agent.spec
 
-cp ~/rpmbuild/RPMS/x86_64/monitoring_agent-1.0-1.x86_64.rpm .
+cp ./packaging/rpmbuild/RPMS/x86_64/monitoring_agent-1.0-1.x86_64.rpm .
 
 echo "RPM build complete!"
