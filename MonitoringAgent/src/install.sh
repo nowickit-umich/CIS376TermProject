2e2cb5c3b35ee7b6b3eae9db434fdcd1ffba925c
unknown <nowickit@umich.edu>
2025-03-29 15:08:31 -0400

    install script test

--- MonitoringAgent/src/install.sh
+++ MonitoringAgent/src/install.sh
@@ -7,6 +7,18 @@ INSTALL="dnf install"
 # install dependencies
 $INSTALL auditd g++
 
+# build 
+g++ ./src/monitoring_agent.cpp -o ./src/monitoring_agent
 
+# config
+cp ./src/monitoring_agent.rules /etc/auditd/rules.d/
+cp ./src/monitoring_agent.service /etc/systemd/system/
 
+# install
+cp ./src/monitoring_agent /usr/bin/
+chmod 755 /usr/bin/monitoring_agent
+
+# enable service
+systemctl enable monitoring_agent
+systemctl start monitoring_agent
 
--- MonitoringAgent/src/monitoring_agent.cpp
+++ MonitoringAgent/src/monitoring_agent.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 int main(){
-    cout << "MONITORING AGENT TEST!" << endl;
+
+    while(1==1){
+        cout << "MONITORING AGENT TEST!" << endl;
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+    
     return 0;
 }
