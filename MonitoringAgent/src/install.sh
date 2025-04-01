3f7ddd37869e0f2f68724dec7f551a85ac89d672
Ted Nowicki <nowickit@umich.edu>
2025-03-31 23:20:59 -0400

    created auditd plugin

--- MonitoringAgent/src/install.sh
+++ MonitoringAgent/src/install.sh
@@ -8,22 +8,29 @@ INSTALL="dnf install"
 $INSTALL g++ libcurl
 
 # build 
-g++ ./src/monitoring_agent.cpp -o ./src/monitoring_agent -lcurl
+g++ ./src/monitoring_agent.cpp -o ./src/agent -lcpr -lcurl
 
-# config
+# config audit rules
 cp ./src/monitoring_agent.rules /etc/audit/rules.d/
-cp ./src/monitoring_agent.service /etc/systemd/system/
-cp ./src/monitoring_agent_auditd.conf /etc/audit/auditd.conf
-#cp ./src/rsyslog_monitoring_agent.conf /etc/rsyslog.d/50-rsyslog_monitoring_agent.conf
+
+#service replaced by plugin
+#cp ./src/monitoring_agent.service /etc/systemd/system/
+
+# auditd plugin
+cp ./src/agent.conf /etc/audit/plugins.d/agent.conf
+
+# audit config
+cp ./src/auditd.conf /etc/audit/auditd.conf
+
 
 
 # install
-cp ./src/monitoring_agent /usr/bin/
-chmod 755 /usr/bin/monitoring_agent
+cp ./src/agent /usr/bin/
+chmod 755 /usr/bin/agent
 
 # enable service
-systemctl enable monitoring_agent
-systemctl start monitoring_agent
+#systemctl enable monitoring_agent
+#systemctl start monitoring_agent
 augenrules --load
 
 
--- MonitoringAgent/src/monitoring_agent.cpp
+++ MonitoringAgent/src/monitoring_agent.cpp
@@ -1,20 +1,66 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include <chrono>
 #include <thread>
-#include <curl/curl.h>
+#include <mutex>
+#include <cpr/cpr.h>
+#include <unistd.h>
+#include <syslog.h>
 
 using namespace std;
 
+static const int BUFFER_SIZE = 128;
+static const string LOG_FILE = "log.txt";
+static const string API = "http://127.0.0.1:5001/submit_logs";
+
+vector<string> buffer;
+mutex buffer_mutex;
+
+void send_logs() {
+	while(true){
+		//spinlock, TODO use semaphore
+		if(buffer.size() < 16) continue;
+
+		string payload = "{\"logs\": \"";
+		buffer_mutex.lock();
+		for (string log : buffer){
+			payload += "<x>" + log + "</x>";
+		}
+		payload += "\"";
+		buffer.clear();
+		buffer_mutex.unlock();
+	
+		auto response = cpr::Post(cpr::Url{API}, cpr::Header{{"Content-Type", "application/json"}}, cpr::Body{payload});
+		
+		syslog(LOG_INFO, "Sent Logs!");
+	}
+}
+
+void monitor_logs(){
+	string log = "";
+	while(true){
+		buffer_mutex.lock();
+		// it may be better to use a non-blocking form of getline
+		while(getline(cin, log) && buffer.size() < BUFFER_SIZE){
+			buffer.push_back(log);
+		}
+		buffer_mutex.unlock();
+	}
+}
+
 int main(){
+	openlog("agentPlugin", LOG_PID | LOG_CONS, LOG_USER);
+	syslog(LOG_INFO, "It Works!");
+	
+	
+	thread send(send_logs);
 
-    CURL* curl = curl_easy_init();
+	monitor_logs();
 
-    while(1==1){
-        cout << "MONITORING AGENT TEST!" << endl;
-        this_thread::sleep_for(chrono::seconds(5));
-    }
+	syslog(LOG_ERR, "It Broke!");
+	closelog();
 
-    curl_easy_cleanup(curl);
-    
-    return 0;
+	return 0;
 }
