3d97d864ec00c34bbc4ed78ce9eda2ebe7f8cc3b
nowickit-umich <159186542+nowickit-umich@users.noreply.github.com>
2025-04-08 20:42:05 -0400

    api call example

--- ManagementServer/frontend/dashboard/src/app/login/page.js
+++ ManagementServer/frontend/dashboard/src/app/login/page.js
@@ -18,7 +18,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:5001/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
