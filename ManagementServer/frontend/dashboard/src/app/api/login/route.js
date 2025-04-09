efc10245fd60d2dcd14cf7083e94ae856415b6d7
pcradu <148838351+pcradu@users.noreply.github.com>
2025-04-08 20:59:46 -0400

    Update route.js

--- ManagementServer/frontend/dashboard/src/app/api/login/route.js
+++ ManagementServer/frontend/dashboard/src/app/api/login/route.js
@@ -3,7 +3,17 @@ import { NextResponse } from 'next/server';
 export async function POST(req) {
     try {
         // "backend" domain name is resolved by docker to the address of the backend container
-        const response = await fetch("http://backend:5001/login");
+        
+        const response = await fetch('backend:5001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: req
+      
+      });
+
+        
         const data = await response.json();
         
         return NextResponse.json(data, { status: 200 });
