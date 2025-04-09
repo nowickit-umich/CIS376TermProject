cef4c6ef4142af74ee103f1bd629ee2f8571b193
nowickit-umich <159186542+nowickit-umich@users.noreply.github.com>
2025-04-08 20:44:05 -0400

    login api example

--- ManagementServer/frontend/dashboard/src/app/api/login/route.js
+++ ManagementServer/frontend/dashboard/src/app/api/login/route.js
@@ -1,1 +1,14 @@
+import { NextResponse } from 'next/server';
 
+export async function POST(req) {
+    try {
+        // "backend" domain name is resolved by docker to the address of the backend container
+        const response = await fetch("http://backend:5001/login");
+        const data = await response.json();
+        
+        return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
