1f4b9ad7f168aa21eb5125b2882bf735d78786cb
David Corona <davico@umich.com>
2025-04-14 21:17:38 -0700

    Hotreload fix

--- ManagementServer/frontend/dashboard/next.config.mjs
+++ ManagementServer/frontend/dashboard/next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  experimental: {
-    appDir: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
   
